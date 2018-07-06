6ed88132092954ff9e40eefd88389497110fb068
wulinyun <lin.wu@landasoft.com>
2018-07-06 18:09:35 +0800

    更新

--- docker.sh
+++ docker.sh
@@ -1,3 +1,6 @@
+#!/bin/bash -ilex
+mvn clean package
+#sh docker.sh
 #构建镜像
 docker build -t="wulinyun/docker-jar-demo" .   
 #查看本地镜像
@@ -6,6 +9,11 @@ docker images
 docker tag wulinyun/docker-jar-demo harbor.landaudev.com/demo/docker-jar-demo:latest
 #推送到私有镜像仓库当中
 docker push harbor.landaudev.com/demo/docker-jar-demo:latest
+#登陆云
+cf login -a https://api.landaudev.com  -u demo -p demo --skip-ssl-validation
+
+#选择组织空间
+cf target -o demo -s deployment
 #部署到PCF
 cf push docker-jar-demo --docker-image harbor.landaudev.com/demo/docker-jar-demo:latest
 #删除tag标记
